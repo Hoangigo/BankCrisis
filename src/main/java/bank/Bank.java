9988b67e24fc7766ac0b009923b4ecfaac8edf50
= <minh.h.b.nguyen@stud.h-da-de>
2023-06-04 19:28:48 +0200

    initial mqtt

--- src/main/java/bank/Bank.java
+++ src/main/java/bank/Bank.java
@@ -7,8 +7,10 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 import org.apache.thrift.TException;
 import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.protocol.TProtocol;
@@ -19,6 +21,11 @@ import org.apache.thrift.transport.TServerTransport;
 import org.apache.thrift.transport.TSocket;
 import org.apache.thrift.transport.TTransport;
 import org.apache.thrift.transport.TTransportException;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 import thrift.BankService;
 import thrift.BankService.Client;
 import thrift.LoanRequest;
@@ -31,7 +38,8 @@ import udphandler.UDPMessage;
  * and communicates using a DatagramSocket.
  */
 public class Bank extends Thread implements Establisher {
-
+    private static final String PREPARE_TOPIC = "prepare";
+    private static final String COMMIT_TOPIC = "commit";
     private final UDPHandler handler;
     private final String bankName;
     private int currentValue;
@@ -45,9 +53,8 @@ public class Bank extends Thread implements Establisher {
     private HashMap<String, Integer> rpcAdress;
     private boolean bankrupt;
     private RPCHelper rpc;
-
-
-
+    private int amount;
+    private MqttClient client;
 
     public Bank(String name, int port, int httpPort) throws IOException {
         this.HTTP_DEFAULT_PORT = httpPort;
@@ -59,6 +66,7 @@ public class Bank extends Thread implements Establisher {
         serverSocket = new ServerSocket(HTTP_DEFAULT_PORT);
         this.bankrupt = false;
         rpc = new RPCHelper(this);
+        amount=0;
         String bankRPCs = System.getenv("RPCBANKS");
         if (bankRPCs != null) {
             rpcAdress= new HashMap<>();
@@ -70,6 +78,12 @@ public class Bank extends Thread implements Establisher {
                 rpcAdress.put(bankName,portName);
             }
         }
+        try{
+            connectMQTT();
+        }
+        catch (MqttException e) {
+            throw new RuntimeException(e);
+        }
         this.handler = new UDPHandler(this) {
             @Override
             public UDPMessage getMessage() {
@@ -77,6 +91,85 @@ public class Bank extends Thread implements Establisher {
                 return new UDPMessage(message);
             }
         };
+    }
+    private void connectMQTT() throws MqttException {
+        client = createClient("broker",1883);
+        client.subscribe("topic");
+        client.setCallback(new MqttCallback() {
+            @Override
+            public void connectionLost(Throwable throwable) {
+                System.out.println("Connection loss");
+            }
+
+            @Override
+            public void messageArrived(String topic, MqttMessage mqttMessage) throws Exception {
+                if (topic.equals(PREPARE_TOPIC)) {
+                    handlePrepareMessage(mqttMessage);
+                } else if (topic.equals(COMMIT_TOPIC)) {
+                    handleCommitMessage(mqttMessage);
+                }
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+
+            }
+        });
+    }
+
+
+    private void handlePrepareMessage(MqttMessage mqttMessage) {
+        String split[] = new String(mqttMessage.getPayload()).split(";");
+        String name = split[0];
+        String money = split[1];
+        if(!name.equals(bankName)){
+            System.out.println(bankName);
+            System.out.println("amount of money"+ money );
+            int amount = Integer.parseInt(money);
+            this.amount= amount;
+            sendMQTTMessage("prepare", name +";"+(amount< this.getCurrentValue()?"true":"false"));
+        }
+    }
+
+    private void handleCommitMessage(MqttMessage mqttMessage) {
+        String commitRequest = new String(mqttMessage.getPayload());
+        if(!commitRequest.equals(bankName)){
+            if(commitRequest.equals("abort")){
+                System.out.println("abort");
+            }
+            else{
+                this.setCurrentValue(this.getCurrentValue()-amount);
+                System.out.println("new value of bank"+ this.getCurrentValue());
+                this.amount=0;
+                sendMQTTMessage("commit", bankName);
+            }
+        }
+    }
+
+
+    public MqttClient createClient(String host,int port){
+        MqttClient publisher = null;
+        String id = UUID.randomUUID().toString();
+        while (publisher== null){
+            try {
+                String ip =InetAddress.getByName(host).toString().substring(host.length()+1);
+                publisher= new MqttClient("tcp://"+ip+":"+port,id);
+
+            } catch (UnknownHostException e) {
+                throw new RuntimeException(e);
+            } catch (MqttException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        while(!publisher.isConnected()){
+            try {
+                publisher.connect();
+            } catch (MqttException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return publisher;
+
     }
 
 
@@ -91,7 +184,8 @@ public class Bank extends Thread implements Establisher {
     public void run() {
         handler.start();
         rpc.start();
-
+        System.out.println("Bank1 send mqtt");
+        sendMQTTMessage("help",this.bankName+";1000");
         try {
             while (running) {
                 if(this.getCurrentValue()<0){
@@ -113,6 +207,17 @@ public class Bank extends Thread implements Establisher {
         }
 
 
+    }
+    private void sendMQTTMessage(String topic, String mess){
+        System.out.println("Send mqtt from Bank with message "+ mess +"with topic "+ topic );
+        try {
+            MqttMessage msg = new MqttMessage();
+            msg.setPayload(mess.getBytes());
+            msg.setQos(0);
+            msg.setRetained(true);
+            client.publish(topic,msg);
+        } catch (MqttException ignore) {
+        }
     }
 
     private void askForHelp() {
---
+++ src/main/java/coordinator/Main.java
@@ -0,0 +1,8 @@
+package coordinator;
+
+public class Main {
+  public static void main(String[] args) {
+    Coordinator coordinator = new Coordinator();
+    coordinator.start();
+  }
+}
